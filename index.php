77515fe5b4e3a54e9019d6d82874f48da17a0b5e
Roccma <mauroroccaf@gmail.com>
2018-05-09 01:36:50 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*******************************************************************************
+
+Este es solo un archivo de inicio para que Heroku despliegue la aplicacion.
+
+*******************************************************************************/
+
+phpinfo(); // Esta funcion mostrara toda la informacion del servidor de PHP
